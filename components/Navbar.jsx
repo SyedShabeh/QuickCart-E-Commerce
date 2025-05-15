089e94d04902c071b04c2add67e8e80016f7e3c0
SyedShabeh <shabehassan1976@gmail.com>
2025-05-15 08:11:44 -0700

    changes the links because of too many bugs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
 "use client"
 import React from "react";
-import { BagIcon, BoxIcon, CartIcon, HomeIcon, assets} from "@/assets/assets";
+import { BagIcon, BoxIcon, CartIcon, HomeIcon, assets} from "../assets/assets";
 import Link from "next/link"
-import { useAppContext } from "@/context/AppContext";
+import { useAppContext } from "../context/AppContext";
 import Image from "next/image";
 import { UserButton, useClerk } from "@clerk/nextjs";
 
