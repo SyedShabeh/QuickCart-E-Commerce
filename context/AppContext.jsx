8a735f10d5812b5cbf17d99e5b5d803a1b648336
SyedShabeh <shabehassan1976@gmail.com>
2025-05-19 06:55:49 -0700

    update app.context file

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -12,7 +12,6 @@ export const AppContextProvider = (props) => {
   const currency = process.env.NEXT_PUBLIC_CURRENCY || "$";
   const router = useRouter();
   const { user } = useUser();
-
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
   const [isSeller, setIsSeller] = useState(true);
